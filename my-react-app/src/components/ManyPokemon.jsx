46fc46b54d539942ef2b525dcba33ab3cb76fa92
Augy Markham <katiefaithmarkham@gmail.com>
2025-06-13 22:06:47 -0700

    Wrote API call

--- my-react-app/src/components/ManyPokemon.jsx
+++ my-react-app/src/components/ManyPokemon.jsx
@@ -1,6 +1,17 @@
 import { Pokemon } from "./Pokemon";
+import { useState, useEffect } from "react";
 
 export function ManyPokemon() {
+    const [pokemonData, setPokemonData] = useState("No Pokemon");
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon-species/?offset=0&limit=20')
+            .then(response => response.json())
+            .then(data => setPokemonData(data));
+
+    }, []);
+
+    console.log(pokemonData);
+
     return(
         <>
             <Pokemon></Pokemon>
